6af6afc272d44d70ef1278a1070cc5c8083da913
mideus <mideus@kth.se>
2018-12-18 11:27:25 +0100

    DBHandler return values

--- Source Packages/controller/Controller.java
+++ Source Packages/controller/Controller.java
@@ -10,26 +10,23 @@ import model.Guides;
 
 public class Controller {
     private static final Controller CONTROLLER = new Controller();
-    
+
     private Controller() {
-        
+
     }
-    
+
     public static Controller getController() {
         return Controller.CONTROLLER;
     }
-    
-    public List<GuideDTO> getGuides() throws Exception{
+
+    public List<GuideDTO> getGuides() throws Exception {
         Guides guides = new Guides();
         return guides.getGuides();
     }
-    
-    public Guide getGuide(GuideDTO guideDTO)throws Exception {
+
+    public Guide getGuide(GuideDTO guideDTO) throws Exception {
         Guide guide = new Guide(guideDTO);
         return guide;
     }
 
-    public void addGuideLanguages(GuideDTO g, LanguageDTO l){
-
-    }
 }
